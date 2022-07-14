73ac8c0ac5223dd8ae876641dc7c04afec5da44b
Yuliya1713 <102380743+Yuliya1713@users.noreply.github.com>
2022-07-14 12:27:54 +0300

    Create v0.2.5-alpha.sh

---
+++ v0.2.5-alpha.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+echo "Начинаем обновление репрозитория "
+echo "-----------------------------------------------------------------------------"
+cd ~/pathfinder/py
+git fetch &>/dev/null
+git checkout v0.2.5-alpha &>/dev/null
+echo "Репозиторий успешно обновлен, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+python3 -m venv .venv &>/dev/null
+source .venv/bin/activate &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt &>/dev/null
+cargo build --release --bin pathfinder &>/dev/null
+sleep 2
+source $HOME/.bash_profile &>/dev/null
+echo "Билд завершен успешно"
+echo "-----------------------------------------------------------------------------"
+systemctl restart starknet
+echo "Нода обновлена и запущена"
+echo "-----------------------------------------------------------------------------"
